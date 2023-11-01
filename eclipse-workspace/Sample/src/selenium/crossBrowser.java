ba0d4055cdcfb2a8f69b623a50c26b1df83d1f9d
nidhi <g.nidhi2015@gmail.com>
2023-11-01 16:47:30 +0000

    new commit

---
+++ eclipse-workspace/Sample/src/selenium/crossBrowser.java
@@ -0,0 +1,39 @@
+package selenium;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+
+public class crossBrowser {
+
+			public static void main(String[] args) throws InterruptedException {
+			//WebDriver driver = new ChromeDriver();
+			//	WebDriver driver = new FirefoxDriver();
+			WebDriver driver = new EdgeDriver();
+			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));// it will apply to all steps and wait for element to load
+			String name="nidhi";
+			driver.get("https://rahulshettyacademy.com/locatorspractice/");
+
+			driver.findElement(By.xpath("//input[@id='inputUsername']")).sendKeys(name);
+			driver.findElement(By.xpath("//input[@placeholder='Password']")).sendKeys("rahulshettyacademy");
+			driver.findElement(By.cssSelector("label[for='chkboxOne']")).click();
+			driver.findElement(By.cssSelector("button[type='submit']")).click();
+			//log in page
+			Thread.sleep(2000);
+			
+			System.out.println(driver.findElement(By.tagName("p")).getText());
+			
+			Assert.assertEquals(driver.findElement(By.tagName("p")).getText(),"You are successfully logged in.");
+					Assert.assertEquals(driver.findElement(By.cssSelector("div[class='login-container'] h2")).getText(),"Hello "+name+",");
+			driver.findElement(By.xpath("//*[text() = 'Log Out'] ")).click();//x-path by text
+			driver.close();
+			
+
+	}
+
+}
