e1df1a409f0bcc4619d3563a7f4dbd3d004ad8a7
nidhi <g.nidhi2015@gmail.com>
2023-11-02 14:56:08 +0000

    Updated dropdown

---
+++ eclipse-workspace/Sample/src/selenium/UpdatedDropdown.java
@@ -0,0 +1,32 @@
+package selenium;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class UpdatedDropdown {
+
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver = new ChromeDriver();
+		//driver.manage().window().maximize();
+		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+		driver.findElement(By.id("divpaxinfo")).click(); // click on passanger dropdown
+		System.out.println(driver.findElement(By.id("divpaxinfo")).getText());
+		
+		for(int i=1;i<5;i++)
+		{
+			driver.findElement(By.id("hrefIncAdt")).click();//click on adult add button
+			
+		}
+		//Thread.sleep(2000);
+		System.out.println(driver.findElement(By.id("divpaxinfo")).getText());
+		
+		
+		driver.findElement(By.id("btnclosepaxoption")).click();// click on done button
+
+		
+		
+		
+
+	}
+
+}
