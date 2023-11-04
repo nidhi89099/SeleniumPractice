eedce9b3e5794f6fe5baaddd317866bb6c8d4352
nidhi <g.nidhi2015@gmail.com>
2023-11-04 11:48:41 +0000

    Alerts handling

---
+++ eclipse-workspace/Sample/src/selenium/Alerts_handling.java
@@ -0,0 +1,31 @@
+package selenium;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class Alerts_handling {
+
+	public static void main(String[] args) {
+
+WebDriver driver = new ChromeDriver();
+driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+//handle Ok alert
+driver.findElement(By.id("name")).sendKeys("nidhi");
+driver.findElement(By.id("alertbtn")).click();
+System.out.println(driver.switchTo().alert().getText());
+driver.switchTo().alert().accept();
+
+//handle ok/cancel alert
+driver.findElement(By.id("name")).sendKeys("nidhi");
+driver.findElement(By.cssSelector("#confirmbtn")).click();
+System.out.println(driver.switchTo().alert().getText());
+driver.switchTo().alert().dismiss();
+
+
+
+
+
+
+	}
+
+}
