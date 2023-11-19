2087f40950477676631e775f362800c57cb8dac6
nidhi <g.nidhi2015@gmail.com>
2023-11-19 11:47:02 +0000

    Listeners in TestNG

---
+++ eclipse-workspace/Sample/src/testNG/Listeners1.java
@@ -0,0 +1,61 @@
+package testNG;
+
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestNGListener;
+import org.testng.ITestResult;
+import org.testng.internal.annotations.IListeners;
+
+public class Listeners1 implements ITestListener{
+
+	@Override
+	public void onTestStart(ITestResult result) {
+		// TODO Auto-generated method stub
+		ITestListener.super.onTestStart(result);
+	}
+
+	@Override
+	public void onTestSuccess(ITestResult result) {
+		//System.out.println("I successfully executeed listeners pass code");
+		ITestListener.super.onTestSuccess(result);
+	}
+
+	@Override
+	public void onTestFailure(ITestResult result) {
+		System.out.println("Ifailed listeners pass code"+result.getName());//to get the name which tc is failed
+		
+		ITestListener.super.onTestFailure(result);
+	}
+
+	@Override
+	public void onTestSkipped(ITestResult result) {
+		// TODO Auto-generated method stub
+		ITestListener.super.onTestSkipped(result);
+	}
+
+	@Override
+	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+		// TODO Auto-generated method stub
+		ITestListener.super.onTestFailedButWithinSuccessPercentage(result);
+	}
+
+	@Override
+	public void onTestFailedWithTimeout(ITestResult result) {
+		// TODO Auto-generated method stub
+		ITestListener.super.onTestFailedWithTimeout(result);
+	}
+
+	@Override
+	public void onStart(ITestContext context) {
+		// TODO Auto-generated method stub
+		ITestListener.super.onStart(context);
+	}
+
+	@Override
+	public void onFinish(ITestContext context) {
+		// TODO Auto-generated method stub
+		ITestListener.super.onFinish(context);
+	}
+
+	
+}
